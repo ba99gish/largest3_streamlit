8a428897c5e76f74de89ef784a594e033cfd06f7
ba99gish <131232415+ba99gish@users.noreply.github.com>
2023-04-19 17:06:16 +0530

    Update largest.py

--- largest.py
+++ largest.py
@@ -7,7 +7,7 @@ Original file is located at
     https://colab.research.google.com/drive/11DkveGIWE8W0B2blSTzC-MpNVCg2MZbg
 """
 
-!pip install -q streamlit
+
 
 import streamlit as st
 import pandas as pd
@@ -23,11 +23,13 @@ def user_input_features():
   num3 = st.number_input("num3", min_value = -2000000000, max_value = 2000000000, step = 1)
 
   numbers = [num1, num2, num3]
-  return numbers
+#   return numbers
 
   numbers = user_input_features()
   res = max(numbers)
 
   st.subheader("Result : the largest of the 3 numbers is ")
-  st.write(res)
+  if st.button('Find Lagrgest Number'):
+    st.write(res)
+  
 
