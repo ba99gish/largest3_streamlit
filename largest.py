6932d349b4eaf3f868009d0c255fe14730bd208f
ba99gish <131232415+ba99gish@users.noreply.github.com>
2023-04-19 17:09:14 +0530

    Update largest.py

--- largest.py
+++ largest.py
@@ -23,13 +23,13 @@ def user_input_features():
   num3 = st.number_input("num3", min_value = -2000000000, max_value = 2000000000, step = 1)
 
   numbers = [num1, num2, num3]
-#   return numbers
+  return numbers
 
-  numbers = user_input_features()
-  res = max(numbers)
+ numbers = user_input_features()
+ res = max(numbers)
 
-  st.subheader("Result : the largest of the 3 numbers is ")
-  if st.button('Find Lagrgest Number'):
+ st.subheader("Result : the largest of the 3 numbers is ")
+ if st.button('Find Lagrgest Number'):
     st.write(res)
   
 
