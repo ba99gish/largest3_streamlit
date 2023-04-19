495ee2c2f5f829695c356d9e366044797613d378
ba99gish <131232415+ba99gish@users.noreply.github.com>
2023-04-19 17:14:27 +0530

    Update largest.py

--- largest.py
+++ largest.py
@@ -12,10 +12,10 @@ Original file is located at
 import streamlit as st
 import pandas as pd
 
-st.write("""
-#Largest number prediction App""")
+st.header("""
+Largest number prediction App""")
 
-st.header('User inputs')
+st.subheader('User inputs')
 
 def user_input_features():
   num1 = st.number_input("num1", min_value = -2000000000, max_value = 2000000000, step = 1)
@@ -27,8 +27,9 @@ def user_input_features():
 numbers = user_input_features()
 res = max(numbers)
 
-st.subheader("Result : the largest of the 3 numbers is ")
+
 if st.button('Find Lagrgest Number'):
+    st.subheader("Result : the largest of the 3 numbers is ")
     st.write(res)
   
 
